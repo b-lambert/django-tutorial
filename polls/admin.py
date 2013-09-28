5b09ba1954096095ab5ce302d944719594bdabd6
Ben Lambert <benjaminhlambert@gmail.com>
2013-09-28 14:59:53 -0700

    made changes to admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Choice, Question
 
-admin.site.register(Poll)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],
+							  'classes': ['collapse']}),
+							   
+    ]
+	inlines = [ChoiceInline]
+	
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
