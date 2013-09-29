a0cc49b7347fe9041d88f1aeda690c50d56dc277
blahshaw <benjaminhlambert@gmail.com>
2013-09-28 22:18:50 -0700

    added some non-admin views

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,24 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render
+
+from polls.models import Question
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the poll index.")
+	latest_poll_list = Question.objects.all().order_by('-pub_date')[:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
 
 def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 	
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." %poll_id)
+	return HttpResponse("You're voting on poll %s." % poll_id)
+	
+def detail(request, poll_id):
+	try:
+		poll = Question.objects.get(pk=poll_id)
+	except Question.DoesNotExist:
+		raise Http404
+	return render(request, 'polls/detail.html', {'poll': poll})
