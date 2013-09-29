9453a6afb78bead1ded6ede581554a61e6cb9b51
Ben Lambert <benjaminhlambert@gmail.com>
2013-09-28 19:38:01 -0700

    added more views

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,13 @@ from django.conf.urls import patterns, url
 
 from polls import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'))
+urlpatterns = patterns('', 
+						# url/polls/
+						url(r'^$', views.index, name='index'),
+						# url/polls/<poll_id>
+						url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+						# url/polls<poll_id>/results
+						url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+						# url/polls/<poll_id>/vote
+						url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+						)
